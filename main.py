f784d107a0145b45a79facd0a6813a57d099ba87
Tianle Xi <txi@callutheran.edu>
2021-09-05 20:59:32 -0700

    chrome options

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ username = os.environ['username']
 password = os.environ['password']
 
 print('Health Check!')
-
-driver = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+#driver = webdriver.Chrome()
 driver.get(url)
 assert "Cal Lutheran Login" in driver.title
 print('logging in')
