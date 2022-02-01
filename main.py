960d97bd10706787d5a8710146ced64f416d977c
Tianle Xi <48536343+xblak@users.noreply.github.com>
2022-02-01 17:57:16 -0500

    update to work with the new site

--- main.py
+++ main.py
@@ -31,11 +31,13 @@ element = WebDriverWait(driver, 10).until(EC.title_is("Daily Health Check"))
 print('filling the form')
 driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[3]/div[2]/fieldset/div[2]/div[1]/button').click()
 # yes
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[1]/div[2]/div[1]/button'))).click()
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[1]/div[2]/button[1]'))).click()
 # no
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[2]/div[2]/div[2]/button'))).click()
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[2]/div[2]/button[3]'))).click()
 # no
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[3]/div[2]/div[2]/button'))).click()
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[3]/div[2]/button[3]'))).click()
+# no 
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'/html/body/div/div[2]/div[3]/div[4]/div[2]/fieldset[4]/div[2]/button[3]'))).click()
 # confirm both
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'//*[@id="confirm_mask"]'))).click()
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, r'//*[@id="confirm"]'))).click()
